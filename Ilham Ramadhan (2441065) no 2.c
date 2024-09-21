e5c059345864ef6de4ab980454b4e255f9cda361
Ilham-Ramadhan-2441065 <ramadhanilham0990@gmail.com>
2024-09-21 05:12:25 -0800

    Add files via upload

---
+++ Ilham Ramadhan (2441065) no 2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+    int panjang = 10; 
+    int lebar = 7;  
+    int luas;
+
+    // Cara 
+    luas = panjang * lebar;
+
+    // Hasil
+    printf("Panjang: %d cm\n", panjang);
+    printf("Lebar: %d cm\n", lebar);
+    printf("Luas Persegi Panjang: %d cm\n", luas);
+
+    return 0;
+}
+
